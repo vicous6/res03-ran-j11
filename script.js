2442ac2ad29cd7da7c59a3ea7c6f086379802a81
vicous6 <vicous@free.fr>
2022-11-22 14:03:34 +0000

    commit de l'apres midi

--- script.js
+++ script.js
@@ -106,6 +106,8 @@ let submitButton3 = document.getElementById("input3")
 
 let form1 = document.getElementById("form1")
 
+let form2 = document.getElementById("form2")
+
 
 // Utilisateur
 let util=document.getElementById("util")
@@ -116,6 +118,14 @@ let email=document.getElementById("email")
 let psw1=document.getElementById("psw1")
 let psw2=document.getElementById("psw2")
 
+// PARTIE 2 du form
+let radios= document.querySelectorAll("#grid1 input")
+let radios2= document.querySelectorAll("#grid2 input")
+
+
+// contient les deux chanps de texte
+let errorResponse=document.querySelectorAll("#form2 p") 
+console.log(p)
 
 
 form1.addEventListener("submit", function(event){
@@ -123,6 +133,7 @@ form1.addEventListener("submit", function(event){
     
   let formOK= true
   
+ 
   email.classList.remove("erreur")
   util.classList.remove("erreur")
   psw1.classList.remove("erreur")
@@ -131,6 +142,7 @@ form1.addEventListener("submit", function(event){
   for(let i=0;i<3;i++){
    
    p[i].innerHTML=""
+   p[i].classList.remove("erreur")
   }
   
   
@@ -142,40 +154,104 @@ form1.addEventListener("submit", function(event){
    formOK=false
    
   }
-   if (email.value.indexOf("@", 0) < 0)                 
+  
+  
+  
+   else if (email.value.indexOf("@", 0) < 0)                 
     { 
         email.classList.add("erreur")
         p[1].innerHTML="Mettez une adresse email valide.";
+       
         formOK=false
     }    
-    if (email.value.indexOf(".", 0) < 0)                 
+   else if (email.value.indexOf(".", 0) < 0)                 
     { 
          email.classList.add("erreur")
         p[1].innerHTML="Mettez une adresse email valide."
+         
         formOK=false
     }    
+ 
   
-  if(psw1.value!= psw2.value ){
+ else if(psw1.value== "" ){
+    
     psw1.classList.add("erreur")
+   p[2].innerHTML="Rentrer une valeur"
+   formOK=false
+   
+  }
+  else if(psw2.value== "" ){
+    psw2.classList.add("erreur")
+     
+   p[2].innerHTML="Rentrer une valeur"
+   formOK=false
+   
+  }
+  
+    
+  else if(psw1.value!= psw2.value ){
+    psw1.classList.add("erreur")
+    psw2.classList.add("erreur")
+     
    p[2].innerHTML="Les mots de passes ne correspondent pas"
    formOK=false
    
   }
-  
-  if(psw1.value== "" ){
-    psw1.classList.add("erreur")
-   p[2].innerHTML="Rentrer une valeur"
-   formOK=false
-   
-  }
-   if(psw2.value== "" ){
-    psw2.classList.add("erreur")
-   p[2].innerHTML="Rentrer une valeur"
-   formOK=false
-   
-  }
-   
-   if(formOK==true){
+   else if(formOK==true){
    event.target.submit();
    }
 });
+
+
+form2.addEventListener("submit",function(event){
+ 
+ event.preventDefault();
+ 
+ 
+ let formOK1=false
+ let formOK2=false
+ errorResponse[0].innerHTML=""
+ errorResponse[1].innerHTML=""
+ 
+ for(let i=0;i<radios.length;i++){
+  
+  if(radios[i].checked==true){
+   
+   formOK1=true
+  }
+ }
+ 
+  for(let i=0;i<radios2.length;i++){
+  
+  if(radios2[i].checked==true){
+   
+   formOK2=true
+  }
+ }
+ 
+ 
+ 
+ // test si le form est ok pour etre submit 
+ 
+ 
+ if(formOK1==false){
+  
+  errorResponse[0].innerHTML="Veuillez choisir une valeur"
+  
+ }
+  
+ if(formOK2==false){
+  
+  errorResponse[1].innerHTML="Veuillez choisir une valeur"
+  
+ }
+ 
+ 
+ if(formOK1==true && formOK2==true){
+  
+  event.target.submit();
+ }
+
+ 
+ 
+})
